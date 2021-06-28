18e58d2ff489f1f62b539fa459bc8a6ba8f37026
CodeCook1e <295385570@qq.com>
2021-06-28 19:39:11 +0800

    init gshop

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,44 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import MSite from '../pages/MSite/MSite.vue'
+import Search from '../pages/Search/Search.vue'
+import Order from '../pages/Order/Order.vue'
+import Profile from '../pages/Profile/Profile.vue'
+import Login from '../pages/Login/Login.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+const routes = [{
+  path: '/',
+  redirect: '/msite'
+}, {
+  path: '/msite',
+  component: MSite,
+  meta: {
+    showFooter: true
   }
-]
+}, {
+  path: '/search',
+  component: Search,
+  meta: {
+    showFooter: true
+  }
+}, {
+  path: '/order',
+  component: Order,
+  meta: {
+    showFooter: true
+  }
+}, {
+  path: '/profile',
+  component: Profile,
+  meta: {
+    showFooter: true
+  }
+}, {
+  path: '/login',
+  component: Login
+}, ]
 
 const router = new VueRouter({
   routes
