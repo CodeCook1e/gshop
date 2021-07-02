9d405c3f2309eee7b684b786d856be797f143e2d
CodeCook1e <295385570@qq.com>
2021-07-02 15:02:52 +0800

    新华外卖项目完成

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -2,69 +2,272 @@
   <!-- on 表示显示，off表示关闭 -->
   <section class="loginContainer">
     <div class="loginInner">
+      <!-- 登录头部 -->
       <div class="login_header">
         <h2 class="login_logo">新华外卖</h2>
         <div class="login_header_title">
-          <a href="javascript:;" class="on">短信登录</a>
-          <a href="javascript:;">密码登录</a>
+          <a
+            href="javascript:;"
+            :class="{ on: loginWay }"
+            @click="loginWay = true"
+          >
+            短信登录
+          </a>
+          <a
+            href="javascript:;"
+            :class="{ on: !loginWay }"
+            @click="loginWay = false"
+          >
+            密码登录
+          </a>
         </div>
       </div>
+      <!-- 登录表单主体 -->
       <div class="login_content">
-        <form>
-          <div class="on">
+        <form @submit.prevent="login">
+          <!-- 短信登录模式 区域 -->
+          <div :class="{ on: loginWay }">
             <section class="login_message">
-              <input type="tel" maxlength="11" placeholder="手机号" />
-              <button disabled="disabled" class="get_verification">
-                获取验证码
+              <input
+                type="tel"
+                maxlength="11"
+                placeholder="手机号"
+                v-model="phone"
+              />
+              <button
+                :disabled="!rightPhone"
+                class="get_verification"
+                :class="{ right_phone: rightPhone }"
+                @click.prevent="getCode"
+              >
+                {{ computeTime > 0 ? `已发送(${computeTime}s)` : "获取验证码" }}
               </button>
             </section>
             <section class="login_verification">
-              <input type="tel" maxlength="8" placeholder="验证码" />
+              <input
+                type="tel"
+                maxlength="8"
+                placeholder="验证码"
+                v-model="code"
+              />
             </section>
             <section class="login_hint">
               温馨提示：未注册新华外卖账号的手机号，登陆时将自动注册，且代表已同意
               <a href="javascript:;">《用户服务协议》</a>
             </section>
           </div>
-          <div>
+          <!-- 密码登录模式 区域 -->
+          <div :class="{ on: !loginWay }">
             <section>
+              <!-- 登录 手机/邮箱/用户名 -->
               <section class="login_message">
                 <input
                   type="tel"
                   maxlength="11"
                   placeholder="手机/邮箱/用户名"
+                  v-model="name"
                 />
               </section>
+              <!-- 登录 密码 -->
               <section class="login_verification">
-                <input type="tel" maxlength="8" placeholder="密码" />
-                <div class="switch_button off">
-                  <div class="switch_circle"></div>
-                  <span class="switch_text">...</span>
+                <!-- 密码可见 -->
+                <input
+                  type="text"
+                  maxlength="8"
+                  placeholder="密码"
+                  v-if="showPwd"
+                  v-model="pwd"
+                />
+                <!-- 密码不可见 -->
+                <input
+                  type="password"
+                  maxlength="8"
+                  placeholder="密码"
+                  v-else
+                  v-model="pwd"
+                />
+                <!-- 密码是否可见的 滑块区域 -->
+                <div
+                  class="switch_button"
+                  :class="showPwd ? 'on' : 'off'"
+                  @click="showPwd = !showPwd"
+                >
+                  <div class="switch_circle" :class="{ right: showPwd }"></div>
+                  <span class="switch_text">{{ showPwd ? "abc" : "..." }}</span>
                 </div>
               </section>
               <section class="login_message">
-                <input type="text" maxlength="11" placeholder="验证码" />
+                <input
+                  type="text"
+                  maxlength="11"
+                  placeholder="验证码"
+                  v-model="captcha"
+                />
                 <img
                   class="get_verification"
-                  src="./images/captcha.svg"
+                  src="http://localhost:4000/captcha"
                   alt="captcha"
+                  @click="getCaptcha"
+                  ref="captcha"
                 />
               </section>
             </section>
           </div>
+          <!-- 登录按钮 -->
           <button class="login_submit">登录</button>
         </form>
         <a href="javascript:;" class="about_us">关于我们</a>
       </div>
+      <!-- 左上角返回箭头 -->
       <a href="javascript:;" class="go_back" @click="$router.back()">
         <i class="iconfont icon-jiantou2"></i>
       </a>
     </div>
+    <!-- 警告弹窗 -->
+    <alter-tip
+      :alertText="alertText"
+      v-show="alertShow"
+      @closeTip="closeTip"
+    ></alter-tip>
   </section>
 </template>
 
 <script>
-export default {};
+import AlterTip from "../../components/AlertTip/AlertTip.vue";
+import { reqSendCode, reqSmsLogin, reqPwdLogin } from "../../api";
+export default {
+  data() {
+    return {
+      loginWay: true, // true 代表短信登录, false 代表密码登录
+      computeTime: 0, // 验证码计时的时间
+      showPwd: false, // 是否显示密码
+      phone: "", // 手机号
+      code: "", // 短信验证码
+      name: "", // 用户名
+      pwd: "", // 密码
+      captcha: "", // 图形验证码
+      alertText: "", // 提示文本
+      alertShow: false, // 是否显示警告框
+    };
+  },
+
+  computed: {
+    // 手机号验证
+    rightPhone() {
+      return /^1\d{10}$/.test(this.phone);
+    },
+  },
+
+  methods: {
+    // 异步获取验证码
+    async getCode() {
+      // 如果当前没有计时
+      if (!this.computeTime) {
+        // 启动倒计时
+        this.computeTime = 30;
+        this.intervalId = setInterval(() => {
+          this.computeTime--;
+          if (this.computeTime <= 0) {
+            // 停止计时
+            clearInterval(this.intervalId);
+          }
+        }, 1000);
+        // 发送 ajax 请求（向指定手机号发送验证码短信）
+        const result = await reqSendCode(this.phone);
+        if (result.code === 1) {
+          // 显示提示
+          this.showAlert(result.msg);
+          // 停止倒计时
+          if (this.computeTime) {
+            this.computeTime = 0;
+            clearInterval(this.intervalId);
+            this.intervalId = undefined;
+          }
+        }
+      }
+    },
+    // 警告框弹出
+    showAlert(alertText) {
+      this.alertShow = true;
+      this.alertText = alertText;
+    },
+    // 异步登录
+    async login() {
+      let result;
+      // 前台表单验证
+      if (this.loginWay) {
+        // 短信登录
+        const { rightPhone, phone, code } = this;
+        if (!this.rightPhone) {
+          // 手机号不正确
+          this.showAlert("手机号不正确");
+          return;
+        } else if (!/^\d{6}$/.test(code)) {
+          // 验证码必须是 6 位数字
+          this.showAlert("验证码必须是 6 位数字");
+          return;
+        }
+        // 发送 ajax 请求短信登陆
+        result = await reqSmsLogin(phone, code);
+      } else {
+        // 密码登录
+        const { name, pwd, captcha } = this;
+        if (!this.name) {
+          // 用户名必须指定
+          this.showAlert("用户名必须指定");
+          return;
+        } else if (!this.pwd) {
+          // 密码必须指定
+          this.showAlert("密码必须指定");
+          return;
+        } else if (!this.captcha) {
+          // 验证码必须指定
+          this.showAlert("验证码必须指定");
+          return;
+        }
+        // 发送 ajax 请求密码登录
+        result = await reqPwdLogin({ name, pwd, captcha });
+      }
+
+      // 停止倒计时
+      if (this.computeTime) {
+        this.computeTime = 0;
+        clearInterval(this.intervalId);
+        this.intervalId = undefined;
+      }
+
+      // 根据结果数据处理
+      if (result.code === 0) {
+        const user = result.data;
+        // 将 user 保存到 vuex 的 state
+        this.$store.dispatch("recordUser", user);
+        // 跳转到个人中心界面
+        this.$router.replace("/profile");
+      } else {
+        // 显示新的图片验证码
+        this.getCaptcha();
+        // 显示警告提示
+        const msg = result.msg;
+        this.showAlert(msg);
+      }
+    },
+    // 警告框关闭
+    closeTip() {
+      this.alertShow = false;
+      this.alertText = "";
+    },
+    // 获取一个新的图片验证码
+    getCaptcha() {
+      // 每次指定的 src 要不一样
+      this.$refs.captcha.src =
+        "http://localhost:4000/captcha?time=" + Date.now();
+    },
+  },
+
+  components: {
+    AlterTip,
+  },
+};
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
@@ -128,6 +331,8 @@ export default {};
               color #ccc
               font-size 14px
               background transparent
+              &.right_phone
+                color black
           .login_verification
             position relative
             margin-top 16px
@@ -156,7 +361,6 @@ export default {};
               &.on
                 background #02a774
               >.switch_circle
-                // transform translateX(27px)
                 position absolute
                 top -1px
                 left -1px
@@ -167,6 +371,8 @@ export default {};
                 background #fff
                 box-shadow 0 2px 4px 0 rgba(0, 0, 0, 0.1)
                 transition transform 0.3s
+                &.right
+                  transform translateX(29px)
           .login_hint
             margin-top 12px
             color #999
---
+++ src/store/mutation-types.js
@@ -0,0 +1,19 @@
+/* 
+  包含 n 个 mutation 的 type 名称常量
+*/
+export const RECEIVE_ADDRESS = 'receive_address' // 接收地址
+export const RECEIVE_CATEGORYS = 'receive_categorys' // 接收食品分类数组
+export const RECEIVE_SHOPS = 'receive_shops' // 接收商家数组
+export const RECEIVE_USER_INFO = 'receive_user_info' // 接收用户信息
+export const RESET_USER_INFO = 'reset_user_info' // 重置用户信息
+
+export const RECEIVE_GOODS = 'receive_goods' // 接收商品数组
+export const RECEIVE_RATINGS = 'receive_ratings' // 接收商家评论数组
+export const RECEIVE_INFO = 'receive_info' // 接收商家信息
+
+export const INCREMENT_FOOD_COUNT = 'increme_food_count' // 增加 food 中的 count
+export const DECREMENT_FOOD_COUNT = 'decreme_food_count' // 减少 food 中的 count
+
+export const CLEAR_CART = 'clear_cart' // 清空购物车
+
+export const RECEIVE_SEARCH_SHOPS = 'receive_search_shops' // 接收搜索的商家数组
